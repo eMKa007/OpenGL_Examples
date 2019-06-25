4b37f1c042b3b51146c85dc14d2026c91880d310
Mike Krzy <krzychusnk@gmail.com>
2019-06-25 21:31:39 +0200

    Linked GLEW and GLM library into project.

--- 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
+++ 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
+	glfwInit();
+
+	//CREATE WINDOW BEFORE GLEW INIT!
+
+	glewExperimental = GL_TRUE;
+
+	if(glewInit() != GLEW_OK)
+	{
+		glfwTerminate();
+		std::cout << "Bad hat harry.." << "\n";
+	}
+	else
+		std::cout << "Good.." << "\n";
 
 
+	system("pause");
 	return 0;
 }
