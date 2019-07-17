6fde91dfa72dfad465d99bd03938895d79f321db
Mike Krzy <krzychusnk@gmail.com>
2019-07-17 20:33:41 +0200

    Texture showing properly. Bug was inisde wertex texture coordinates.

--- 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
+++ 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
@@ -21,7 +21,7 @@ Vertex vertices[] =
 	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	// Vertex 0
 	glm::vec3(-0.5f, -0.5f, 0.f),	glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),	// Vertex 1
 	glm::vec3(0.5f, -0.5f, 0.f),	glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	// Vertex 2
-	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 0.f)		// Vertex 3
+	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(1.f, 1.f)		// Vertex 3
 	
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
