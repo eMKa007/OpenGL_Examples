f5ac4bc7d23adbcc45d3b95e05aa69fc8ec68faf
Mike Krzy <krzychusnk@gmail.com>
2019-07-17 19:46:11 +0200

    Removed redundant vertices and reused valid ones.

--- 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
+++ 1_OpenGL-Cpp_Tutorial/1_OpenGL-Cpp_Tutorial/main.cpp
@@ -18,13 +18,10 @@ void updateInput( GLFWwindow* window )
 Vertex vertices[] =
 {
 	// Position						// Color					// texcoord
-	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	// Vertex 1
-	glm::vec3(-0.5f, -0.5f, 0.f),	glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),	// Vertex 2
-	glm::vec3(0.5f, -0.5f, 0.f),	glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	// Vertex 3
-
-	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	// Vertex 1
-	glm::vec3(0.5f, -0.5f, 0.f),	glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	// Vertex 3
-	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 0.f)		// Vertex 4
+	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	// Vertex 0
+	glm::vec3(-0.5f, -0.5f, 0.f),	glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),	// Vertex 1
+	glm::vec3(0.5f, -0.5f, 0.f),	glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	// Vertex 2
+	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 0.f)		// Vertex 3
 	
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
@@ -35,7 +32,7 @@ unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 GLuint indices[] =
 {
 	0, 1, 2,	// Triangle 1
-	3, 4, 5		// Triangle 2
+	0, 2, 3		// Triangle 2
 };
 unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
