0c412397214e72df3162b031fc04a35cb552c01d
Mike Krzy <krzychusnk@gmail.com>
2019-09-18 17:47:02 +0200

    Created new project file. Libaries linked properly.

---
+++ 2_Bilard_Balls/2_Bilard_Balls/libs.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+/* OpenGL Extension Wrangler */
+#include <glew.h>
+
+#include <glfw3.h>
+
+/* GLM - OpenGL Mathematics */
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+/* SOIL2 - Simple OpenGL Image Library */
+#include <SOIL2.h>
---
+++ 2_Bilard_Balls/2_Bilard_Balls/main.cpp
@@ -0,0 +1,8 @@
+#include "libs.h"
+
+int main()
+{
+	std::cout << "Here we go!!!" << std::endl;
+
+	return 0;
+}
